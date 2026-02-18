cb80ecd11ef2759905f87fbc467d1f79db1fca91
Mikel538 <mikel538.work@gmail.com>
2026-02-18 19:57:29 +0100

    fix: vite.config base for pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   // base: process.env.NODE_ENV === "production" ? "" : "",
+  base: "/Mikel-Fullstack-Developer-Portfolio/",
 });
