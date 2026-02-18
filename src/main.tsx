73e94168a69fe5ec8be250b21ddffb93346123bb
Mikel538 <mikel538.work@gmail.com>
2026-02-18 19:46:38 +0100

    fix: fix for page reload (HashRouter), and images routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./i18n/i18n";
 import "./styles/main.min.css";
 import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Mikel-Fullstack-Developer-Portfolio/">
+    {/* <BrowserRouter basename="/Mikel-Fullstack-Developer-Portfolio/"> */}
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
+    {/* </BrowserRouter> */}
   </React.StrictMode>,
 );
--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base:
-    process.env.NODE_ENV === "production"
-      ? "/Mikel-Fullstack-Developer-Portfolio/"
-      : "/Mikel-Fullstack-Developer-Portfolio/",
+  // base: process.env.NODE_ENV === "production" ? "" : "",
 });
